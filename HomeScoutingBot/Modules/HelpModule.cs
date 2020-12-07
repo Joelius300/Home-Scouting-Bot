110bc67b00015841000be1e6182c9bdf5139c996
Joelius300 <bstlng321@gmail.com>
2020-12-07 19:14:13 +0100

    Add scaffolding for HelpModule with links

---
+++ HomeScoutingBot/Modules/HelpModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace HomeScoutingBot.Modules
+{
+    public class HelpModule : ModuleBase<SocketCommandContext>
+    {
+        // https://github.com/Aux/Discord.Net-Example/blob/2.0/src/Modules/HelpModule.cs
+        // https://github.com/TheRealHona/Discord.NetBotTemplate/blob/master/src/DiscordBotTemplate/Utilities/HelpCommandUtilities.cs
+    }
+}
