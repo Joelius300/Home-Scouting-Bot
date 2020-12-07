89c040be806ee242be1dd5f9f381303dc7540092
Joelius300 <bstlng321@gmail.com>
2020-12-07 19:13:11 +0100

    Add basic Setup and Teardown command for groups
    Not so sure about the TextOptions yet
    Also I'm not sure about the rate-limit (specifiying the RetryMode)

--- HomeScoutingBot/Modules/GroupModule.cs
+++ HomeScoutingBot/Modules/GroupModule.cs
@@ -27,31 +27,76 @@ namespace HomeScoutingBot.Modules
             _logger = logger;
         }
 
+        [Command(nameof(Setup))] // Should probably be RunMode Async but for that the scoping stuff need to be reworked
+        [RequireBotPermission(GuildPermission.ManageChannels | GuildPermission.SendMessages)]
+        public async Task Setup(int amount)
+        {
+            if (amount < 2)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+
+            RequestOptions requestOptions = new RequestOptions
+            {
+                RetryMode = RetryMode.RetryRatelimit
+            };
+
+            for (int i = 1; i <= amount; i++)
+            {
+                string name = string.Format(_groupConfig.GroupChannelNameTemplate, i);
+                ICategoryChannel category = await Context.Guild.CreateCategoryChannelAsync(name, options: requestOptions);
+
+                Action<GuildChannelProperties> assignCategoryId = c => c.CategoryId = category.Id;
+                await Context.Guild.CreateTextChannelAsync(name, assignCategoryId, requestOptions);
+                await Context.Guild.CreateVoiceChannelAsync(name, assignCategoryId, requestOptions);
+            }
+
+            await ReplyAsync(string.Format(_textConfig.GroupsCreated, amount));
+        }
+
         [Command(nameof(Teardown))]
         [RequireBotPermission(GuildPermission.ManageChannels | GuildPermission.SendMessages)]
         public async Task Teardown()
         {
+            // TODO Maybe confirm with user
+
+            RequestOptions requestOptions = new RequestOptions
+            {
+                RetryMode = RetryMode.RetryRatelimit
+            };
+
+            int count = 0;
+            foreach (SocketGuildChannel channel in GetAllGroupChannels())
+            {
+                await channel.DeleteAsync(requestOptions);
+                if (channel is ICategoryChannel)
+                {
+                    count++;
+                }
+            }
+
+            await ReplyAsync(string.Format(_textConfig.GroupsDeleted, count));
+        }
+
+        // Returns Voice, Text AND Categories
+        private IEnumerable<SocketGuildChannel> GetAllGroupChannels()
+        {
             const string Placeholder = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // regex-safe placeholder used for allowing regex-unsafe name-templates
-
-            await ReplyAsync("Tearing down..");
-
-            // TODO Potentially find an easy way to confirm using some other message
 
             string nameRegex = string.Format(_groupConfig.GroupChannelNameTemplate, Placeholder);
             nameRegex = Regex.Escape(nameRegex);
             nameRegex = nameRegex.Replace(Placeholder, "\\d+");
 
             Predicate<string> matchName = name => Regex.IsMatch(name, $"^{nameRegex}$", RegexOptions.IgnoreCase);
+            IEnumerable<SocketCategoryChannel> groupCategories = Context.Guild.CategoryChannels
+                                                                              .Where(c => matchName(c.Name));
 
-            IEnumerable<SocketCategoryChannel> groupCategories = Context.Guild.CategoryChannels.Where(c => matchName(c.Name));
             foreach (SocketCategoryChannel categoryChannel in groupCategories)
             {
                 foreach (SocketGuildChannel channel in categoryChannel.Channels)
                 {
-                    await channel.DeleteAsync();
+                    yield return channel;
                 }
 
-                await categoryChannel.DeleteAsync();
+                yield return categoryChannel;
             }
         }
     }
--- HomeScoutingBot/Options/TextOptions.cs
+++ HomeScoutingBot/Options/TextOptions.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 
 namespace HomeScoutingBot.Options
 {
+    /// <summary>
+    /// Contains format strings to build replies to the user.
+    /// </summary>
     public class TextOptions
     {
         /// <summary>
         /// A format string which takes the username and the error message.
         /// </summary>
         public string CommandExecutionFailed { get; set; } = "Sorry {0}, something went wrong: {1}";
+
+        /// <summary>
+        /// A format string which takes the number of groups deleted.
+        /// </summary>
+        public string GroupsDeleted { get; set; } = "{0} groups deleted.";
+
+        /// <summary>
+        /// A format string which takes the number of groups created.
+        /// </summary>
+        public string GroupsCreated { get; set; } = "{0} groups added.";
     }
 }
